c8978d0a62f07a86f1112ea90898d08d2c191048
Will Kaspar <Will@Aang.local>
2020-01-08 12:53:26 -0600

    defines EditScore component

---
+++ src/components/edit-score.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditScore extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to EditScore Component</p>
+            </div>
+        )
+    }
+}
