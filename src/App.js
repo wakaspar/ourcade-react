5bdb6144cf162ae6011efc5eb1c750c93766d41c
Will Kaspar <Will@Aang.local>
2020-01-08 12:45:53 -0600

    adds <Router> element to src/App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class App extends Component {
   render() {
     return (
+      <Router>
         <div className="container">
           <h2>Welcome to Ourcade React!</h2>
         </div>
+      </Router>
     );
   }
 }
