63b31edff4f303f4d795176d61688641437ff064
Will Kaspar <Will@Aang.local>
2020-01-08 12:53:43 -0600

    defines ScoresList component

---
+++ src/components/scores-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ScoresList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to ScoresList Component</p>
+            </div>
+        )
+    }
+}
