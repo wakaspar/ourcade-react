c1eb6e1d9953a3cadccc98df8e62081ffbf60e52
Will Kaspar <Will@Aang.local>
2020-01-14 10:46:51 -0600

    adds mongoose.connect statement to backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Connect to Database
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+// db sanity check
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
